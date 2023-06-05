9af6f2301af4713dadf752973f46150db8fcb484
ruslan.kulik <05kra01@vnr.aa.local>
2023-06-05 13:31:39 +0300

    change sender

--- src/sender.cpp
+++ src/sender.cpp
@@ -3,25 +3,44 @@
 Sender::Sender()
 {
     //here reading data by channel from .conf file
+    string name;
+    string ip;
+    uint32_t port;
+    PROT prot;
+    createChannel(name, ip, port, prot);
+
 }
 
-unique_ptr<Connection> Sender::getChannel()
+Connection* Sender::getChannel(const string& name)
 {
-    //select anyone channel
-    return {};
+    //select anyone channel by name
+    auto found = connections.find(name);
+    if(found != connections.end())
+        return found->second.get();
+    else
+        return nullptr;
 }
 
-void Sender::createChannel(const string& ip, uint32_t port, PROT prot)
+
+void Sender::createChannel(const string& name, const string& ip, uint32_t port, PROT prot)
 {
     switch(prot)
     {
     case PROT::UPD:
-            connections.push_back( make_unique<ConnectionUdp>(ip,port));
+    {
+        //connections[name]= make_unique<ConnectionUdp>(ip,port);
+        unique_ptr<Connection> conn = make_unique<ConnectionUdp>(ip, port);
+        connections.insert(std::make_pair(name, std::move(conn)));
         break;
+    }
 
     case PROT::TCP:
-            connections.push_back( make_unique<ConnectionTcp>(ip,port));
+    {
+        //connections[name]= make_unique<ConnectionTcp>(ip,port);
+        unique_ptr<Connection> conn = make_unique<ConnectionTcp>(ip, port);
+        connections.insert(std::make_pair(name, std::move(conn)));
         break;
+    }
 
     default:
             std::cout << "Invalid solution" <<std::endl;
